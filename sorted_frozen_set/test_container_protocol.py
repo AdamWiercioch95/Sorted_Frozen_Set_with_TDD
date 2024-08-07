f2b10bb8f502558ce04ca9d84929eb80c0137b23
Adam Wiercioch <adamwiercioch95@gmail.com>
2024-08-07 10:27:26 +0200

    containter protocol test

--- sorted_frozen_set/test_container_protocol.py
+++ sorted_frozen_set/test_container_protocol.py
@@ -1,3 +1,5 @@
+from collections.abc import Container
+
 import pytest
 
 from sorted_frozen_set.sorted_frozen_set import SortedFrozenSet
@@ -22,3 +24,7 @@ def test_positive_not_contained(sorted_frozen_set):
 
 def test_negative_not_contained(sorted_frozen_set):
     assert not (6 not in sorted_frozen_set)
+
+
+def test_container_protocol():
+    assert issubclass(SortedFrozenSet, Container)
