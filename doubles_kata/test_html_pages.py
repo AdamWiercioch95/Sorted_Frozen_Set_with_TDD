6b1de59f623e392b3d1cc5069c1e152107200689
Adam Wiercioch <adamwiercioch95@gmail.com>
2024-08-13 17:49:36 +0200

    fake test for html pages coverter - access second page

--- doubles_kata/test_html_pages.py
+++ doubles_kata/test_html_pages.py
@@ -9,3 +9,14 @@ def test_convert_quotes():
     html_conv = HtmlPagesConverter(open_file=fake_file)
     converted_text = html_conv.get_html_page(0)
     assert converted_text == "quote: &#x27;<br />"
+
+
+def test_access_second_page():
+    fake_file = io.StringIO("""page 1
+ PAGE_BREAK
+ page 2
+ PAGE_BREAK
+ page 3""")
+    html_conv = HtmlPagesConverter(open_file=fake_file)
+    converted_text = html_conv.get_html_page(1)
+    assert converted_text == "page 2<br />"
